2a76004301231021174f9ffa8923c948be673eb9
Kyle <Kyle>
2014-03-08 15:07:45 -0500

    Terrain generated

--- tanks.py
+++ tanks.py
@@ -28,8 +28,9 @@ class Player:
 
 class Terrain_dx:
     """Definition for one dx of the terrain.  Terrain is generated from lots of thin slits."""
-    def __init__(self,x,height):
+    def __init__(self,x,y,height):
         self.x=x
+        self.y=y
         self.height=height
         
 
@@ -37,25 +38,29 @@ class Tanks_Model:
     """Encodes the game state"""
     def __init__(self):
         self.phase=1 # Decides whose gets to move first.
+        self.terrain=[]
         for x in range(640):
-            new_dx=Terrain_dx(x,random.randint(10,30))
+            new_dx=Terrain_dx(x,0,random.randint(10,200))
             self.terrain.append(new_dx)
             
 class PyGameView:
     """Renders model data to the window"""
-    def init(self,model,screen):
+    def __init__(self,model,screen):
         self.model = model
         self.screen = screen
     
-    def draw (self,size):
-        self.screen.fill(pygame.color(0,0,0))
-        x_size=size[0]
-        y_size=size[0]
-        for 
-    
+    def draw (self):
+        self.screen.fill(pygame.Color(0,0,0))
+        for dx in self.model.terrain:
+            pygame.draw.rect(self.screen,pygame.Color(255,255,255),pygame.Rect(dx.x,480-dx.height,1,dx.height))
+            print('dx')
+        pygame.display.update()
 class Controller:
     def __init__(self,model):
         self.model=model
+        
+    def handle_pygame_event(self,event):
+        return
 
 if __name__ == '__main__':
         pygame.init()
@@ -67,10 +72,10 @@ if __name__ == '__main__':
         running = True
         while running:
             for event in pygame.event.get():
-                if even.type == QUIT:
+                if event.type == QUIT:
                     running = False
                 controller.handle_pygame_event(event)
-            model.update()
-            view.draw(size)
+            #model.update()
+            view.draw()
             time.sleep(.001)
-        pygame.quit
+        pygame.quit()
